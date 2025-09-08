da980136e2a358a8c2945315cc92f4b03437808e
Andre Fukuda <andre.alex.fukuda@gmail.com>
2025-09-08 19:38:24 -0300

    transformers monitors

--- QGIS-python-BusMarkers.py
+++ QGIS-python-BusMarkers.py
@@ -3,9 +3,11 @@ import os
 #UCBT = 'D:\\py-dss-bdgd2dss\\Cemig-D_4950_2022-12-31_V11_20230920-1643.gdb|layername=UCBT_tab'
 UNTRMT = 'D:\py-dss-bdgd2dss\Cemig-D_4950_2022-12-31_V11_20230920-1643.gdb|layername=UNTRMT'
 #SSDBT = 'D:\py-dss-bdgd2dss\Cemig-D_4950_2022-12-31_V11_20230920-1643.gdb|layername=SSDBT'
+#SSDMT = 'D:\py-dss-bdgd2dss\Cemig-D_4950_2022-12-31_V11_20230920-1643.gdb|layername=SSDMT'
 
 #ucbt_table = QgsVectorLayer(UCBT,'UCBT_2022','ogr')
 untrtm_table = QgsVectorLayer(UNTRMT,'UNTRTM_2022','ogr')
+
 
 
 
@@ -28,7 +30,8 @@ with open(os.path.join(path, filename), 'w') as file:
 for col in untrtm_table.getFeatures(request):
     
  
-    text = ''.join(('AddBusMarker Bus=', col['PAC_1'],' code=15 color=Red size=4','\n'))
+    text = ''.join(('AddBusMarker Bus=', col['PAC_1'],' code=15 color=Red size=4','\n',
+                    'New monitor.PMONITOR', col['COD_ID'] ,' element=transformer.TRF_', col['COD_ID'] , 'A terminal=1 mode=1 ppolar=no','\n','\n'))
     
     print(text)
     
@@ -37,10 +40,10 @@ for col in untrtm_table.getFeatures(request):
         
     
     count += 1
-    '''
+    
     if count >= 3:
         break
-    '''
+    
     
 
 
