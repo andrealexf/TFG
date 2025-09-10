c62595a53e5e6249af87815d988c9971eb9978f6
Andre Fukuda <andre.alex.fukuda@gmail.com>
2025-09-10 20:17:16 -0300

    BRR table's COD_ID comparison to UNTRMT table's COD_ID

--- QGIS-python-BRRTransformers.py
+++ QGIS-python-BRRTransformers.py
@@ -4,9 +4,15 @@ path = r'C:\Users\Andre\Downloads\TFG\Desenvolvimento-SegundoSemestre\QGIS-OPEND
 layer = 'D:\\py-dss-bdgd2dss\\UNTRMTJOINs|layername=UNTRMTJOIN-AREARURAL'
 table = QgsVectorLayer(layer,'UNTRTMJOINs','ogr')
 
+untrmtLayer = 'D:\\py-dss-bdgd2dss\\Cemig-D_4950_2022-12-31_V11_20230920-1643.gdb|layername=UNTRMT'
+untrmtTable = QgsVectorLayer(untrmtLayer,'UNTRTM','ogr')
+
+filter_expression = "\"CTMT\" = 'IJAU11'"
+request = QgsFeatureRequest().setFilterExpression(filter_expression)
+
 filename=''
 
-def createFile(name, tab):
+def createFile(name, brr, tr):
 
     if not os.path.exists(path):
         os.makedirs(path)
@@ -16,36 +22,36 @@ def createFile(name, tab):
 
     with open(os.path.join(path, filename), 'w') as file:
         file.write("")
-    
-    
-    count = 0
+        
+    #count = 0
+    transformerCount = 0
     print(path)
     print(filename)
     
-    vist = set()
-    
-    for feat in tab.getFeatures():
-        
-        pac2 = feat['PAC_2']
-        
-        if pac2 in vist:
-            continue 
-        
-        vist.add(pac2)
-    
-        text = ''.join(('AddBusMarker Bus=', pac2,' code=15 color=Red size=4','\n',
-                        'New monitor.PMONITOR', str(f"{feat['COD_ID']:.0f}") ,' element=transformer.TRF_',
-                        str(f"{feat['COD_ID']:.0f}") , 'A terminal=1 mode=1 ppolar=no','\n','\n'))
-        print(text)
-        with open(os.path.join(path, filename), 'a') as file:
-            file.write(str(text).strip("'"))
-    
-        count += 1
-        '''
-        if count >= 3:
-            break
-        '''
+    codIdBRR = {str(f"{feat['COD_ID']:.0f}") for feat in brr.getFeatures()} #guarda os COD_ID da tabela de bairro
+    #print(codIdBRR)
 
+    for feat in tr.getFeatures(request):
+    
+        if feat['COD_ID'] in codIdBRR:
+            
+            transformerCount += 1
+           
+            text = ''.join(('AddBusMarker Bus=', feat['PAC_1'],' code=15 color=Red size=4 !buses=[',feat['PAC_1'],' ', 
+                            feat['PAC_2'],']' ,'\n',
+                            'New monitor.PMONITOR', feat['COD_ID'] ,' element=transformer.TRF_',
+                            feat['COD_ID'] , 'A terminal=1 mode=1 ppolar=no','\n','\n'))
+
+            
+            with open(os.path.join(path, filename), 'a') as file:
+                file.write(str(text).strip("'"))
+                
+                
+                
+    with open(os.path.join(path, filename), 'a') as file:
+        file.write('\n')
+        file.write('!Numero de transformadores no bairro: '+str(transformerCount))
+    
 def getBRR(brr):
         bairro = {
             "0": "AREARURAL",
@@ -67,9 +73,9 @@ def getBRR(brr):
     
         return bairro.get(brr, "Bairro não encontrado")
 
-for i in range(1):
+for i in range(14):
 
     layer = 'D:\\py-dss-bdgd2dss\\UNTRMTJOINs|layername=UNTRMTJOIN-'+getBRR(str(i))
     table = QgsVectorLayer(layer,'UNTRTMJOINs','ogr')
-    createFile(getBRR(str(i)), table)
+    createFile(getBRR(str(i)), table, untrmtTable)
    
