b0b0e8a0c37743144f7e3ada0612836f80e337e7
Andre Fukuda <andre.alex.fukuda@gmail.com>
2025-09-18 19:31:25 -0300

    BRR addition

--- ODSS-COM-python.py
+++ ODSS-COM-python.py
@@ -10,35 +10,52 @@ import numpy as np
 import matplotlib as mpl
 import pandas as pd
 import os
+import DSSStartup as dsss
+
 
 os.chdir('C://Users//Andre//Downloads//TFG//Desenvolvimento-SegundoSemestre//QGIS-OPENDSS//IJAU11')
 mydir=os.getcwd()
 print(mydir)
 
-import DSSStartup as dsss  # DSSStartup is a function for starting up the DSS; must be downloaded and stored in the working folder
 
-# create de DSS object
-DSSStartOK, DSSObj, DSSText = dsss.DSSStartup()    # DSSObj is the DSS object
+DSSStartOK, DSSObj, DSSText = dsss.DSSStartup()
 if not DSSStartOK:
     print('Unable to start the OpenDSS Engine')
     import sys
     sys.exit()
 
 DSSCircuit = DSSObj.ActiveCircuit
-
 DSSSolution = DSSCircuit.Solution
+DSSActiveBus = DSSCircuit.ActiveBus
+DSSText = DSSObj.Text
 ControlQueue = DSSCircuit.CtrlQueue
 
 DSSObj.AllowForms = 0
-DSSText = DSSObj.Text
 
 DSSText.Command = 'clear'
-
 DSSText.Command = 'Compile ' + mydir + '/Master_DU01_2022124950_IJAU11_--MBS-1--T--.dss'
+
 DSSText.Command = 'Redirect ' + mydir + '/BRR_AREARURAL.dss'
+
+'''
+DSSText.Command = 'Redirect ' + mydir + '/BRR_AVENIDA.dss'
+DSSText.Command = 'Redirect ' + mydir + '/BRR_BOAVISTA.dss'
+DSSText.Command = 'Redirect ' + mydir + '/BRR_BPS.dss'
+DSSText.Command = 'Redirect ' + mydir + '/BRR_CENTRO.dss'
+DSSText.Command = 'Redirect ' + mydir + '/BRR_CRUZEIRO.dss'
+DSSText.Command = 'Redirect ' + mydir + '/BRR_ESTIVA.dss'
+DSSText.Command = 'Redirect ' + mydir + '/BRR_MORROCHIC.dss'
+DSSText.Command = 'Redirect ' + mydir + '/BRR_NOSSASENHORADAAGONIA.dss'
+DSSText.Command = 'Redirect ' + mydir + '/BRR_PINHEIRINHO.dss'
+DSSText.Command = 'Redirect ' + mydir + '/BRR_SANTOANTONIO.dss'
+DSSText.Command = 'Redirect ' + mydir + '/BRR_SAOVICENTE.dss'
+DSSText.Command = 'Redirect ' + mydir + '/BRR_UNIFEI.dss'
+DSSText.Command = 'Redirect ' + mydir + '/BRR_VILAISABEL.dss'
+'''
+
 DSSSolution.Solve()
 
-DSSActiveBus = DSSCircuit.ActiveBus
+
 
 DSSCircuit.SetActiveBus('981764')
 DSSCircuit.SetActiveElement('Line.smt_2621843')
@@ -48,8 +65,8 @@ DSSLines = DSSCircuit.Lines
 Monitors = DSSCircuit.Monitors.AllNames
 print('List of Monitors')
 print ('Total:' +  str(len(Monitors)))
-print(Monitors)
-#DSSText.Command = "Plot monitor object=TOTALpower channels=(1 3 5)"
+#print(Monitors)
+DSSText.Command = "Plot monitor object=TOTALpower channels=(1 3 5)"
 
 #- Circuit Summary -
 
@@ -57,8 +74,10 @@ kW_BC = DSSCircuit.TotalPower[0] #in kW
 kvar_BC = DSSCircuit.TotalPower[1] #in kVAr
 kW_Loss_BC = DSSCircuit.Losses[0]/1000 #property returns values in W, here stored in kW
 kvar_Loss_BC = DSSCircuit.Losses[1]/1000 #property returns values in VAr, here stored in kVAr
+print("----------")
 print("Total kW delivered to the network: %.3f kW" %kW_BC)
 print("Total kVAr delivered to the network: %.3f kvar" %kvar_BC)
 print("Total active losses: %.3f kW" %kW_Loss_BC)
 print("Percent losses: %.3f" %abs(((kW_Loss_BC/kW_BC)*100)) + "%")
 print("Total reactive losses: %.3f kVAr" %kvar_Loss_BC)
+print("----------")
