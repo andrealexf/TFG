4afc293c17c6a9c1dbb7c1421fe4a6e406e137af
Andre Fukuda <andre.alex.fukuda@gmail.com>
2025-09-21 15:04:27 -0300

    Next Element (Topology)

--- ODSS-COM-python.py
+++ ODSS-COM-python.py
@@ -25,8 +25,11 @@ if not DSSStartOK:
     sys.exit()
 
 DSSCircuit = DSSObj.ActiveCircuit
+
 DSSSolution = DSSCircuit.Solution
 DSSActiveBus = DSSCircuit.ActiveBus
+DSSCktElement = DSSCircuit.ActiveCktElement
+DSSTopology = DSSCircuit.Topology
 DSSText = DSSObj.Text
 ControlQueue = DSSCircuit.CtrlQueue
 
@@ -54,18 +57,53 @@ DSSText.Command = 'Redirect ' + mydir + '/BRR_VILAISABEL.dss'
 '''
 
 DSSSolution.Solve()
+'''
+linha = "smt_2621843"
+LineActive = DSSTopology.SetBranchActive(f"Line.{linha}")
+assert LineActive, "No line"
+'''
 
+alvo = "Line.smt_2621843".lower()   # a sua linha X
 
+ok = DSSTopology.First > 0
+found = False
+while ok:
+    if DSSTopology.BranchName.lower() == alvo:
+        found = True
+        break
+    ok = DSSTopology.Next > 0
+
+if not found:
+    raise RuntimeError("Ramo 'Line.X' não encontrado na Topology")
+#-----------------------------------------------------------
+if DSSTopology.ForwardBranch > 0: #vai para o próximo elemento
+
+    prox_nome = DSSTopology.BranchName
+    print("Próximo elemento: ",prox_nome)
+
+else:
+    print("Não há ramo")
 
 DSSCircuit.SetActiveBus('981764')
 DSSCircuit.SetActiveElement('Line.smt_2621843')
+#print(DSSCktElement.Name)
+
 DSSLines = DSSCircuit.Lines
 
+DSSCircuit.SetActiveClass('Transformer')
+lineList = []
+
+print(DSSLines.Bus1)
+print(DSSCircuit.NextElement)
+#print(DSSCircuit.Transformers.Next)
+#print(DSSLines.Next)
+#i = DSSCircuit.SetActiveBus('981764')
+
+#print(DSSActiveBus.LineList)
 
 Monitors = DSSCircuit.Monitors.AllNames
-print('List of Monitors')
-print ('Total:' +  str(len(Monitors)))
-#print(Monitors)
+
+print (str(len(Monitors)) + ' Monitors')
 DSSText.Command = "Plot monitor object=TOTALpower channels=(1 3 5)"
 
 #- Circuit Summary -
@@ -74,6 +112,7 @@ kW_BC = DSSCircuit.TotalPower[0] #in kW
 kvar_BC = DSSCircuit.TotalPower[1] #in kVAr
 kW_Loss_BC = DSSCircuit.Losses[0]/1000 #property returns values in W, here stored in kW
 kvar_Loss_BC = DSSCircuit.Losses[1]/1000 #property returns values in VAr, here stored in kVAr
+'''
 print("----------")
 print("Total kW delivered to the network: %.3f kW" %kW_BC)
 print("Total kVAr delivered to the network: %.3f kvar" %kvar_BC)
@@ -81,3 +120,4 @@ print("Total active losses: %.3f kW" %kW_Loss_BC)
 print("Percent losses: %.3f" %abs(((kW_Loss_BC/kW_BC)*100)) + "%")
 print("Total reactive losses: %.3f kVAr" %kvar_Loss_BC)
 print("----------")
+'''
