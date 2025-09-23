19e36dc7be589dc252ae84ff5e71b90e198c98ab
Andre Fukuda <andre.alex.fukuda@gmail.com>
2025-09-23 19:32:26 -0300

    createGD function

--- ODSS-COM-python.py
+++ ODSS-COM-python.py
@@ -5,9 +5,11 @@
 !pip install matplotlib
 !pip install dss-python
 '''
+import math
 
 import numpy as np
 import matplotlib as mpl
+import math
 import pandas as pd
 import os
 import DSSStartup as dsss
@@ -58,6 +60,15 @@ DSSText.Command = 'Redirect ' + mydir + '/BRR_VILAISABEL.dss'
 
 DSSSolution.Solve()
 
+nome_arquivo = "ramal.txt"
+
+diretorio = os.path.dirname(os.path.abspath(__file__))
+arquivo = os.path.join(diretorio, nome_arquivo)
+
+with open(arquivo, "r", encoding="utf-8") as f:
+    conteudo = f.readlines()
+
+
 #--------------------------------------
 
 def defineBranchName(alvo: str) -> bool:
@@ -75,17 +86,17 @@ def getLoads(transformer):
     defineBranchName(transformer)
     global bus1
     global bus2
-    teste = []
+    loadIpList = []
     loadList = []
 
     while ((DSSTopology.BranchName.split(".", 1)[1]).split("_", 1)[0])[:3] != "smt":  # enquanto line não for smt...
 
         bus1 = DSSCktElement.BusNames[0]
         bus2 = DSSCktElement.BusNames[1]
-        print(DSSTopology.BranchName)
+        #print(DSSTopology.BranchName)
         if busLoads(bus2):
-            print(" ", busLoads(bus2))
-            teste.append(busLoads(bus2))
+            #print(" ", busLoads(bus2))
+            loadIpList.append(busLoads(bus2))
 
             if (busLoads(bus2)[0].split("_", 1)[1])[:2] != "ip":
                 loadList.append(busLoads(bus2))
@@ -94,9 +105,32 @@ def getLoads(transformer):
         DSSTopology.ForwardBranch
 
     defineBranchName(ramal)
-    print("Número de cargas no transformador:", alvo, " :", len(teste), teste)
-    print("Número de cargas no transformador (excluindo iluminação):", len(loadList), loadList)
-    return bus1, bus2
+    #print("Número de cargas no transformador:", alvo, " :", len(loadIpList), loadIpList)
+    #print("Número de cargas no transformador (excluindo iluminação):", len(loadList), loadList)
+    return loadList
+
+def createGD(loadList: list):
+
+    for i in range(len(loadList)):
+
+        encontrar = ((loadList[i])[0]).split("_")[1]
+
+        with open(arquivo, "r", encoding="utf-8") as f:
+            conteudo = f.readlines()
+
+        for linha in conteudo:
+            if linha.split(" ", 2)[1] == encontrar:
+
+                ramal = linha.split(" ", 2)[1]
+                energia = float(linha.split(" ")[3])
+                pot = energia/(24*30*0.17)
+                kva = math.ceil(pot)
+
+                print("Nome do ramal: ", ramal, " -- Média de Energia: ", energia)
+                DSSCircuit.SetActiveElement("load."+(loadList[i])[0])
+                bus = str(DSSCktElement.BusNames).strip("(',')")
+                print('New "PVsystem.GD.BT.'+ramal+'" phases=1 bus1='+bus+' conn=Delta kv=0.22 pf=0.92 pmpp='+f"{pot:.2f}"+' kva='+f"{kva:.2f}"+' irradiance=0.98')
+                print('~ temperature=25 %cutin=0.1 %cutout=0.1 effcurve=Myeff P-TCurve=MyPvsT Daily=PVIrrad_diaria TDaily=MyTemp')
 
 def busLoads(bus_base: str): #cargas conectadas à barra
     alvo = norm(bus_base)
@@ -117,19 +151,16 @@ def norm(b):
     return (b or "").split(".", 1)[0].lower()
 
 alvo = "transformer.TRF_1081464A".lower()
-getLoads(alvo)#obtem as cargas conectadas a um transformador
+loadList = getLoads(alvo)#obtem as cargas conectadas a um transformador
+createGD(loadList)
 
+'''
 print("")
 print("Active Element:",DSSCktElement.Name)
 print("Branch Name:",DSSTopology.BranchName)
 print("")
 print("Nome da ultima linha: ", DSSTopology.BranchName,'\n', "Bus2: ", bus2,'\n', "Quantidade de cargas conectadas: ", len(busLoads(bus2)), busLoads(bus2))
-
-
-#Monitors = DSSCircuit.Monitors.AllNames
-#print (str(len(Monitors)) + ' Monitors')
-
-#- Circuit Summary -
+'''
 
 kW_BC = DSSCircuit.TotalPower[0] #in kW
 kvar_BC = DSSCircuit.TotalPower[1] #in kVAr
@@ -144,3 +175,5 @@ print("Percent losses: %.3f" %abs(((kW_Loss_BC/kW_BC)*100)) + "%")
 print("Total reactive losses: %.3f kVAr" %kvar_Loss_BC)
 print("----------")
 '''
+
+print('fim')
