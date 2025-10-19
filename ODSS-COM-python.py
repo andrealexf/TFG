a2be0b7f88401f503dcda5fbbf8561ad3366179c
Andre Fukuda <andre.alex.fukuda@gmail.com>
2025-10-19 03:20:02 -0300

    hc script part1

--- ODSS-COM-python.py
+++ ODSS-COM-python.py
@@ -69,9 +69,10 @@ def norm(b):
 def getLoads(transformer):
     defineBranchName(transformer)
     print(DSSTopology.BranchName)
+
     global bus1
     global bus2
-    #loadIpList = []
+
     loadList = []
     DSSTopology.ForwardBranch
 
@@ -80,93 +81,40 @@ def getLoads(transformer):
         bus1 = DSSCktElement.BusNames[0]
         bus2 = DSSCktElement.BusNames[1]
         print("")
-        print(DSSTopology.BranchName)
         voltageBus(bus1)
-        voltageBus(bus2)
 
         if busLoads(bus2):
-            print(" ", busLoads(bus2))
-            #loadIpList.append(busLoads(bus2))
 
-            if (busLoads(bus2)[0].split("_", 1)[1])[:2] != "ip":
-                #print(" ", busLoads(bus2))
-                loadList.append(busLoads(bus2))
+            m1m2 = busLoads(bus2)
+            print(" ", m1m2) #envia como bus2 mas carga só possui uma bus (na função está como bus1)
 
-                load = busLoads(bus2)[0]
-                voltageBus(bus2, load)
+            if (m1m2[0].split("_", 1)[1])[:2] != "ip":
+                #print(" ", m1m2)
+                loadList.append(m1m2)
+
+                load = m1m2[0]
+                #voltageBus(bus2, load)
+
+                if voltageBus(bus2, load):
+                    overVoltageList.append(m1m2)
+
 
         ramal = DSSTopology.BranchName
         DSSTopology.ForwardBranch
 
     loadList = list(chain.from_iterable(loadList))
+
     defineBranchName(ramal)
     print("")
-    # print("Número de cargas no transformador:", alvo, " :", len(loadIpList), loadIpList)
     print("Número de cargas no transformador (excluindo iluminação):", len(loadList), loadList)
-    #print("")
+
     return loadList
-
-'''
-def createGD(nomeBairro, loadList: list, mult: float = 1, limpar: bool = False):
-
-    txt = nomeBairro + "-mult" + str(mult) + "-pv.dss"
-    # txt = "APAGAR-pv.dss"
-    pvdss = pv_dir / txt
-
-    mode = "w" if limpar else "a"
-
-    with pvdss.open(mode, encoding="utf-8") as f:
-        f.write("")
-
-    PVnumber = 0
-    norepeat = {}
-
-    for i in range(len(loadList)):
-
-        encontrar = ((loadList[i])[0]).split("_")[1]
-
-        with open(arquivo, "r", encoding="utf-8") as f:
-            conteudo = f.readlines()
-
-        for linha in conteudo:
-            if linha.split(" ", 2)[1] == encontrar:
-                ramal = linha.split(" ", 2)[1]
-                energia = float(linha.split(" ")[3])
-
-                if energia == 0:
-                    continue
-
-                else:
-                    pvname = ((loadList[i])[0]).split("_")[1]
-                    norepeat[pvname] = norepeat.get(pvname, 0) + 1
-                    pvname = pvname if norepeat[pvname] == 1 else f"{pvname}_{norepeat[pvname]}"
-
-                    pot = (energia) / (24 * 30 * 0.17)
-                    pot = pot * mult
-                    kva = math.ceil(pot)
-
-                    DSSCircuit.SetActiveElement("load." + (loadList[i])[0])
-                    bus = str(DSSCktElement.BusNames).strip("(',')")
-                    pn = phasesNumber(bus)
-
-                    txt = 'New "PVsystem.GD.BT.' + pvname + '" phases=' + pn + ' bus1=' + bus + ' conn=Delta kv=0.22 pf=0.92 pmpp=' + f"{pot:.2f}" + ' kva=' + f"{kva:.2f}" + ' irradiance=0.98' + '\n' + '~ temperature=25 %cutin=0.1 %cutout=0.1 effcurve=Myeff P-TCurve=MyPvsT Daily=PVIrrad_diaria TDaily=MyTemp' + '\n' + '\n'
-
-                    with pvdss.open("a", encoding="utf-8") as f:
-                        f.write(txt)
-
-                    #print('New "PVsystem.GD.BT.' + pvname + '" phases=' + pn + ' bus1=' + bus + ' conn=Delta kv=0.22 pf=0.92 pmpp=' + f"{pot:.2f}" + ' kva=' + f"{kva:.2f}" + ' irradiance=0.98'+'\n')
-                    #print('~ temperature=25 %cutin=0.1 %cutout=0.1 effcurve=Myeff P-TCurve=MyPvsT Daily=PVIrrad_diaria TDaily=MyTemp'+"\n")
-                    #print('')
-                    PVnumber += 1
-
-
-    print(PVnumber)
-'''
 
 def createGD2(nomeBairro, loadList: list[cargaBT], mult: float = 1.0, limpar: bool = False):
 
-    txt = nomeBairro + "-mult" + str(mult) + "-pv.dss"
-    # txt = "APAGAR-pv.dss"
+    #txt = nomeBairro + "-mult" + str(mult) + "-pv.dss"
+    txt = 'trf167839-hc-pv.dss'
+
     pvdss = pv_dir / txt
 
     mode = "w" if limpar else "a"
@@ -247,7 +195,10 @@ def voltageBus(bus1, load=None):
         pu = DSSCircuit.ActiveBus.puVmagAngle
         pu_round = tupleFormat(pu, casas=4)
         print("     (p.u., ang): ", pu_round)
-        #overvoltage(pu_round)
+
+        if overvoltage(pu_round):
+            #overVoltageList.append(load)
+            return True
 
     else: #linhas e transformadores
 
@@ -256,7 +207,7 @@ def voltageBus(bus1, load=None):
         pu = DSSCircuit.ActiveBus.puVmagAngle
         pu_round = tupleFormat(pu, casas=4)
         print("(p.u., ang): ", pu_round)
-        #overvoltage(pu_round)
+        overvoltage(pu_round)
 
 def findLoad(loadlist: list) -> list[cargaBT]: #obtem kw e o tipo de curva da carga
 
@@ -299,11 +250,16 @@ def tupleFormat(tupla, casas=4):
     return tuple(round(x, casas) for x in tupla)
 
 def overvoltage(tupla):
-    encontrados = []
 
-    for i, valor in enumerate(tupla):
-        if i % 2 == 0 and valor > 1.02:
-            print("Valor maior que 1.02")
+    global contadorOvervoltage
+
+    if any(valor > 1.05 for valor in tupla[::2]):
+        print("Valor maior que 1.05")
+        contadorOvervoltage += 1
+        return True
+
+    else:
+        return False
 
 def verboseSolve(datapath):
     DSSText.Command = 'set datapath="' + datapath + '"'
@@ -329,14 +285,17 @@ def defineBranchName(alvo: str) -> bool:
 
     return False
 
-def solvetrf(alvo, mult):
+def solvetrf(alvo):
 
-    txt = '/trf167839-mult' + str(mult) + '-pv.dss'
+    #txt = '/trf167839-mult' + str(mult) + '-pv.dss'
+    txt = '"trf167839-hc-pv.dss"'
 
-    DSSText.Command = 'Compile ' + mydir + txt
+    DSSText.Command = 'set datapath="C:\\Users\\Andre\\Downloads\\TFG\\Desenvolvimento-SegundoSemestre\\BRR-PVSyst"'
+    DSSText.Command = 'redirect '+ txt
+
+    DSSText.Command = 'set datapath="C:\\Users\\Andre\\Downloads\\TFG\\Desenvolvimento-SegundoSemestre\\QGIS-OPENDSS\\IJAU11"'
     DSSText.Command = 'set mode=snapshot'
     DSSText.Command = 'solve'
-    getLoads(alvo)
 
 pv_dir = Path('C://Users//Andre//Downloads//TFG//Desenvolvimento-SegundoSemestre//BRR-PVSyst')
 
@@ -386,42 +345,43 @@ for j in range(1):
     #verboseSolve(datapath)
 '''
 
-
+contadorOvervoltage = 0
 
 alvo="Transformer.trf_167839a".lower() #transformador linha 7067 do isolated: trf_1081464a (). trf_166467a linha 26044 (165 cargas)
 #loadList = getLoads(alvo)
-#loadList = ['bt_11266978_m1', 'bt_11266978_m2', 'bt_1050133_m1', 'bt_1050133_m2', 'bt_1050126_m1', 'bt_1050126_m2', 'bt_1050112_m1', 'bt_1050112_m2', 'bt_10406755_m1', 'bt_10406755_m2', 'bt_1050105_m1', 'bt_1050105_m2', 'bt_1050098_m1', 'bt_1050098_m2', 'bt_1050091_m1', 'bt_1050091_m2', 'bt_1050077_m1', 'bt_1050077_m2', 'bt_4892503_m1', 'bt_4892503_m2', 'bt_1050070_m1', 'bt_1050070_m2', 'bt_1050056_m1', 'bt_1050056_m2', 'bt_1050049_m1', 'bt_1050049_m2', 'bt_1050063_m1', 'bt_1050063_m2', 'bt_3640476_m1', 'bt_3640476_m2', 'bt_1050084_m1', 'bt_1050084_m2', 'bt_3885504_m1', 'bt_3885504_m2', 'bt_1050042_m1', 'bt_1050042_m2', 'bt_1050035_m1', 'bt_1050035_m2', 'bt_1050028_m1', 'bt_1050028_m2', 'bt_1050021_m1', 'bt_1050021_m2', 'bt_1050014_m1', 'bt_1050014_m2', 'bt_1050007_m1', 'bt_1050007_m2', 'bt_1050000_m1', 'bt_1050000_m2', 'bt_4301661_m1', 'bt_4301661_m2', 'bt_4127207_m1', 'bt_4127207_m2', 'bt_1049993_m1', 'bt_1049993_m2', 'bt_1049986_m1', 'bt_1049986_m2', 'bt_1049979_m1', 'bt_1049979_m2', 'bt_1049972_m1', 'bt_1049972_m2', 'bt_11382069_m1', 'bt_11382069_m2', 'bt_10214815_m1', 'bt_10214815_m2', 'bt_1049965_m1', 'bt_1049965_m2', 'bt_1049958_m1', 'bt_1049958_m2', 'bt_1049951_m1', 'bt_1049951_m2', 'bt_1049937_m1', 'bt_1049937_m2', 'bt_1049930_m1', 'bt_1049930_m2', 'bt_1049923_m1', 'bt_1049923_m2', 'bt_1049916_m1', 'bt_1049916_m2', 'bt_4496954_m1', 'bt_4496954_m2', 'bt_3998960_m1', 'bt_3998960_m2', 'bt_1049909_m1', 'bt_1049909_m2', 'bt_1049902_m1', 'bt_1049902_m2']
-#cargasBTList = findLoad(loadList)
+loadList = ['bt_11266978_m1', 'bt_11266978_m2', 'bt_1050133_m1', 'bt_1050133_m2', 'bt_1050126_m1', 'bt_1050126_m2', 'bt_1050112_m1', 'bt_1050112_m2', 'bt_10406755_m1', 'bt_10406755_m2', 'bt_1050105_m1', 'bt_1050105_m2', 'bt_1050098_m1', 'bt_1050098_m2', 'bt_1050091_m1', 'bt_1050091_m2', 'bt_1050077_m1', 'bt_1050077_m2', 'bt_4892503_m1', 'bt_4892503_m2', 'bt_1050070_m1', 'bt_1050070_m2', 'bt_1050056_m1', 'bt_1050056_m2', 'bt_1050049_m1', 'bt_1050049_m2', 'bt_1050063_m1', 'bt_1050063_m2', 'bt_3640476_m1', 'bt_3640476_m2', 'bt_1050084_m1', 'bt_1050084_m2', 'bt_3885504_m1', 'bt_3885504_m2', 'bt_1050042_m1', 'bt_1050042_m2', 'bt_1050035_m1', 'bt_1050035_m2', 'bt_1050028_m1', 'bt_1050028_m2', 'bt_1050021_m1', 'bt_1050021_m2', 'bt_1050014_m1', 'bt_1050014_m2', 'bt_1050007_m1', 'bt_1050007_m2', 'bt_1050000_m1', 'bt_1050000_m2', 'bt_4301661_m1', 'bt_4301661_m2', 'bt_4127207_m1', 'bt_4127207_m2', 'bt_1049993_m1', 'bt_1049993_m2', 'bt_1049986_m1', 'bt_1049986_m2', 'bt_1049979_m1', 'bt_1049979_m2', 'bt_1049972_m1', 'bt_1049972_m2', 'bt_11382069_m1', 'bt_11382069_m2', 'bt_10214815_m1', 'bt_10214815_m2', 'bt_1049965_m1', 'bt_1049965_m2', 'bt_1049958_m1', 'bt_1049958_m2', 'bt_1049951_m1', 'bt_1049951_m2', 'bt_1049937_m1', 'bt_1049937_m2', 'bt_1049930_m1', 'bt_1049930_m2', 'bt_1049923_m1', 'bt_1049923_m2', 'bt_1049916_m1', 'bt_1049916_m2', 'bt_4496954_m1', 'bt_4496954_m2', 'bt_3998960_m1', 'bt_3998960_m2', 'bt_1049909_m1', 'bt_1049909_m2', 'bt_1049902_m1', 'bt_1049902_m2']
+cargasBTList = findLoad(loadList)
 #createGD2("trf167839", cargasBTList,2,limpar=True)#REFAZER PARA O TRANSFORMADOR DO EXEMPLO
 
-for i in range(3):
-    solvetrf(alvo, mult2.get(i))
-    #print(mult2.get(i))
+overVoltageList = []
+rodando = True
+multwhile = 1.0
+
+while rodando: #percorre os elementos (getLoads) e adiciona PV nas cargas enquanto sua tensão é < 1.05 pu
+
+    createGD2("trf167839-hc", cargasBTList, 1.5, limpar=True)
+    solvetrf(alvo)
+    getLoads(alvo)
+    overVoltageList = list(chain.from_iterable(overVoltageList))  #novo cargaBTList
+
+    cargasBTList = overVoltageList
+    print("Contador OverVoltage: ", contadorOvervoltage)
+
+    if contadorOvervoltage > 200:
+        rodando = False
+
+    multwhile += 0.1
+
+print("Número de barras com tensão maior que 1.05 pu: ", contadorOvervoltage)
+
 
 '''
 DSSText.Command = 'Compile ' + mydir + '/trf167839-mult2-pv.dss'
 DSSText.Command = 'set mode=snapshot'
 DSSText.Command = 'solve'
 loadListAPAGAR = getLoads(alvo)
-
-DSSText.Command = 'set datapath ="C://Users//Andre//Downloads//TFG//Desenvolvimento-SegundoSemestre//Resultados//Verbose//"'
-DSSText.Command = 'set mode=daily'
-DSSText.Command = 'set number=24'
-DSSText.Command = 'set DemandInterval=true'
-DSSText.Command = 'set overloadreport=true'
-DSSText.Command = 'set voltexceptionreport=true'
-DSSText.Command = 'set DIVerbose=true'
-DSSText.Command = 'solve'
-DSSText.Command = 'closeDI'
 '''
 
-'''
-print("")
-print("Active Element:",DSSCktElement.Name)
-print("Branch Name:",DSSTopology.BranchName)
-print("")
-print("Nome da ultima linha: ", DSSTopology.BranchName,'\n', "Bus2: ", bus2,'\n', "Quantidade de cargas conectadas: ", len(busLoads(bus2)), busLoads(bus2))
-'''
 
 kW_BC = DSSCircuit.TotalPower[0]  # in kW
 kvar_BC = DSSCircuit.TotalPower[1]  # in kVAr
